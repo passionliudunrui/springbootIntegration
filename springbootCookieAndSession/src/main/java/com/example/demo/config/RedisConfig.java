898feba380de397f92d28bb09d3bb02ba4bb195d
2390634566@qq.com <2390634566@qq.com>
2022-05-26 11:59:06 +0800

    增加cookie和session

---
+++ springbootCookieAndSession/src/main/java/com/example/demo/config/RedisConfig.java
@@ -0,0 +1,40 @@
+package com.example.demo.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+
+import javax.annotation.Resource;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        //key的序列化方法
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        //value设置序列化的方式
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        //设置hash类型的序列化
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        //设置连接工厂
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        return redisTemplate;
+
+    }
+
+}
--- springbootCookieAndSession/src/main/java/com/example/demo/controller/UserController.java
+++ springbootCookieAndSession/src/main/java/com/example/demo/controller/UserController.java
@@ -2,14 +2,19 @@ package com.example.demo.controller;
 
 import com.example.demo.domain.User;
 import com.example.demo.mapper.UserMapper;
+import com.example.demo.utils.CookieUtil;
+import com.example.demo.utils.UUIDUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
 @Controller
@@ -19,9 +24,14 @@ public class UserController {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+//    @Autowired
+//    private StringRedisTemplate stringRedisTemplate;
+
     @RequestMapping("/dologin")
 //    @ResponseBody
-    public String login(HttpServletRequest request){
+    public String login(HttpServletRequest request, HttpServletResponse response){
         String uname = request.getParameter("username");
         String password = request.getParameter("password");
         log.info("uname:{}",uname);
@@ -31,6 +41,13 @@ public class UserController {
 
         if(user != null){
             System.out.println("11111");
+            String ticket= UUIDUtil.uuid();
+            redisTemplate.opsForValue().set("user:"+ticket,user);
+
+            CookieUtil.setCookie(request,response,"userTicket",ticket);
+
+
+
             return "test.html";
         }else{
             System.out.println("22222222222");
@@ -54,4 +71,40 @@ public class UserController {
             return "registN";
         }
     }
+
+    @ResponseBody
+    @RequestMapping("/doSomething")
+    public String something(HttpServletRequest request,HttpServletResponse response){
+
+        System.out.println("进入");
+
+        String ticket=CookieUtil.getCookieValue(request,"userTicket");
+        if(ticket==null){
+            System.out.println("not ticket");
+        }
+
+        User user=(User)redisTemplate.opsForValue().get("user:"+ticket);
+        System.out.println(user);
+        if(user==null){
+            System.out.println("not user");
+        }
+
+//        String uname = request.getParameter("username");
+//        String password = request.getParameter("password");
+//        String password2 = request.getParameter("password2");
+//        log.info("uname:{}",uname);
+//        log.info( "pasword:{}",password );
+//        log.info( "pasword2:{}",password2 );
+//        User user = userMapper.selectUname(uname);
+//        if(user == null && password.equals(password2)){
+//            userMapper.saveUser(uname,password);
+//            return "index";
+//        }else{
+//            return "registN";
+//        }
+
+        return "你有权限查看";
+    }
+
+
 }
